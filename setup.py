704c8522b542c0faf30c430f8c185bd986ea86d6
Aditya Sawarkar <adisawarkar.20@gmail.com>
2025-09-17 16:20:41 +0530

    requirements.added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='aditya',
+    author_email='adityadhanraj@priaccinnovations.ai',
+    packages=find_packages(),
+    install_requires=[]
+)
